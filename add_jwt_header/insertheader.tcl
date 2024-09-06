97bc9137876ff6809517367656d79ffc0a8b088d
Peter Scheffler <p.scheffler@f5.com>
2024-09-06 15:54:32 -0400

    Made some edits to clean up the code Added the -secure flag on the ACCESS::data get

--- add_jwt_header/insertheader.tcl
+++ add_jwt_header/insertheader.tcl
@@ -1,5 +1,6 @@
 when RULE_INIT priority 100 {
-    set jwt "UNSET"
+
+    # set static::jwt "UNSET"
 }
 
 when ACCESS_POLICY_AGENT_EVENT priority 100 {
@@ -7,11 +8,11 @@ when ACCESS_POLICY_AGENT_EVENT priority 100 {
     set pspid [ACCESS::policy agent_id]
     log local0. "PSP iRuleID $pspid"
 
-    if { $pspid eq "add_jwt" } {
-        set jwt [ACCESS::session data get "session.oauth.client.last.id_token"]
+    if { $pspid eq "psp_add_jwt" } {
+        ACCESS::session data set "session.custom.userdata.jwt" [ACCESS::session data get -secure "session.oauth.client.last.id_token"]
         set path [HTTP::path]
 
-        log local0. "Adding [string range $jwt 0 15] on $path"
+        log local0. "Adding [string range [ACCESS::session data get "session.custom.userdata.jwt"] 0 15] on $path"
     }
 
 }
@@ -23,11 +24,11 @@ when ACCESS_PER_REQUEST_AGENT_EVENT priority 100 {
 
     if { $prpid eq "prp_add_jwt" } {
 
-        set jwt [ACCESS::session data get "session.oauth.client.last.id_token"]
+        # set jwt [ACCESS::session data get "session.oauth.client.last.id_token"]
         set path [HTTP::path]
 
-        HTTP::header insert "Authorization" "Bearer $jwt"
-        log local0. "Adding JWT ([string range $jwt 0 15]) to Authorization header on $path"
+        HTTP::header insert "Authorization" [ACCESS::session data get "session.custom.userdata.jwt"]
+        log local0. "Adding JWT ([string range [ACCESS::session data get "session.custom.userdata.jwt"] 0 15]) to Authorization header on $path"
     }
 }
 
